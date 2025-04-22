1f03717c4c007e46faae9c65d9738b112ba62dce
justin <kinhanjustin@gmail.com>
2025-04-22 16:54:13 +0200

    Navbar deleted

--- Taalmodel/Client/src/App.jsx
+++ Taalmodel/Client/src/App.jsx
@@ -1,9 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./Home.jsx";
-import Layout from "./Layout.jsx";
 
 const router = createBrowserRouter([{
-    element: <Layout />,
     children: [
         {
             path: "/",
